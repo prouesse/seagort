cd5524e2ca91d1f1ca2ffa7971b8a91ad39a59d9
Nike <nikechukwu@gmail.com>
2024-03-06 19:24:05 -0500

    Updated Rust: TODO needs rebuild

--- utils/engine/src/utils/c_interface.rs
+++ utils/engine/src/utils/c_interface.rs
@@ -7,16 +7,12 @@ pub unsafe fn ruststring_to_cstring(arg: &str) -> *const c_char {
     // Get a raw pointer to the underlying bytes
     let c_ptr: *const c_char = c_string.as_ptr();
 
-    println!("C String {:?}", unsafe { CStr::from_ptr(c_ptr) });
-
     c_string.into_raw()
 }
 
 pub unsafe fn cchar_to_ruststr(word: *const c_char) -> String {
     let var = unsafe { CStr::from_ptr(word) };
     let printable = var.to_string_lossy().into_owned();
-
-    println!("Rust String {}", printable);
 
     printable
 }
@@ -27,8 +23,6 @@ pub unsafe fn cchar_to_ruststaticstr(word: *const c_char) -> &'static str {
 
     let c_string = CString::new(printable).expect("Failed to convert to CString");
     let static_str = c_string.into_string().expect("Failed to convert to String");
-
-    println!("Rust String {}", static_str);
 
     Box::leak(static_str.into_boxed_str())
 }
