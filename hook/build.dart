34a6643706450b2b905d334ffa53afda2b2b58fa
Nike <nikechukwu@gmail.com>
2024-04-14 12:38:08 +0000

    Added code

---
+++ hook/build.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+import 'package:native_assets_cli/native_assets_cli.dart';
+import 'package:native_toolchain_c/native_toolchain_c.dart';
+
+const assetName = 'libseagort.so';
+final packageAssetPath = Uri.file('bridge/linux/$assetName');
+
+void main(List<String> args) async {
+  await build(args, (config, output) async {
+    final packageName = config.packageName;
+    final assetPath = config.outputDirectory.resolve(assetName);
+    final assetSourcePath = config.packageRoot.resolveUri(packageAssetPath);
+    if (!config.dryRun) {
+      // Insert code that downloads or builds the asset to `assetPath`.
+      await File.fromUri(assetSourcePath).copy(assetPath.toFilePath());
+
+      output.addDependencies([
+        assetSourcePath,
+        config.packageRoot.resolve('hook/build.dart'),
+      ]);
+    }
+
+    output.addAsset(
+      NativeCodeAsset(
+        package: packageName, 
+        name: assetName, 
+        file: assetPath,
+        linkMode: DynamicLoadingBundled(), 
+        os: config.targetOS,
+        architecture: config.targetArchitecture,
+      )
+    );
+  });
+}
